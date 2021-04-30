b890ee60f5c99ad34fd1935c099b4bfbf03293a1
DimasShofaGunarso <dimas.gunarso@gmail.com>
2021-04-30 14:55:05 +0700

    login

--- app/src/main/java/com/dimas/login18090061/Register.kt
+++ app/src/main/java/com/dimas/login18090061/Register.kt
@@ -1,11 +1,25 @@
 package com.dimas.login18090061
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class Register : AppCompatActivity() {
+class Register : AppCompatActivity() , View.OnClickListener {
+    private  lateinit var btnIntent: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        btnIntent = findViewById(R.id.backregister)
+        btnIntent.setOnClickListener(this)
+    }
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.backlogin -> run {
+                val intenBiasa = Intent(this@Register, MainActivity::class.java)
+                startActivity(intenBiasa)
+            }
+        }
     }
 }
